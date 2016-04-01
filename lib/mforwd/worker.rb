fcfe2a33d149276ae7456673785c09423479eef9
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-01 09:29:02 +0900

    Add Worker class includes Sidekiq::Worker

---
+++ lib/mforwd/worker.rb
@@ -0,0 +1,20 @@
+require 'sidekiq'
+
+require 'mforwd'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :namespace => 'x', :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :namespace => 'x' }
+end
+
+class MForwd::Worker
+  include Sidekiq::Worker
+
+  def perform(how_hard="super hard", how_long=1)
+    sleep how_long
+    puts "Workin' #{how_hard}"
+  end
+end
