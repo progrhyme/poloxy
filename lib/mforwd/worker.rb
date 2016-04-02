c3be4728427c50dbd1dab12c1393625566b620f9
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-02 09:25:38 +0900

    Worker invokes every interval

--- lib/mforwd/worker.rb
+++ lib/mforwd/worker.rb
@@ -3,18 +3,39 @@ require 'sidekiq'
 require 'mforwd'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'x', :size => 1 }
+  config.redis = {
+    url:       'redis://localhost:6379',
+    namespace: 'mforwd',
+    size:      1
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'x' }
+  config.redis = {
+    url:       'redis://localhost:6379',
+    namespace: 'mforwd'
+  }
 end
 
 class MForwd::Worker
   include Sidekiq::Worker
+  @@queued       = 0
+  @@invoked      = 0
+  @@last_invoked = 0
+  @@min_interval = 60
 
-  def perform(how_hard="super hard", how_long=1)
-    sleep how_long
-    puts "Workin' #{how_hard}"
+  def perform obj
+    @@queued += 1
+    p "#{@@queued}, #{obj.to_s}"
+    if (Time.now - @@last_invoked).to_i >= @@min_interval
+      invoke
+      @@queued = 0
+    end
+  end
+
+  def invoke
+    @@invoked += 1
+    @@last_invoked = Time.now
+    p "invoked at #{@@last_invoked.to_s}"
   end
 end
