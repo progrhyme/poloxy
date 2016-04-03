1745f6ed3a615c2fe306a86d9b409c31b1b16eed
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-03 12:51:47 +0900

    Wait some time for additional enqueue when queue detected.

--- lib/mforwd/worker.rb
+++ lib/mforwd/worker.rb
@@ -5,23 +5,33 @@ class MForwd::Worker
     @config = MForwd::Config.new
     @buffer = MForwd::Buffer.new config: @config, role: :server
     @item_merger = MForwd::Item::Merge.new config: @config.deliver['item']
-    @interval = 5 # DEVELOP
-    #@interval = 15
+    @interval = 5
   end
 
   def run
     Signal.trap :INT, sighandler(:shutdown)
     @running = true
-    @cnt = 0
+    @waiting = 0
     while @running
-      @cnt += 1
       data = @buffer.pop_all
+      if data.empty?
+        @waiting += 1
+        p "#{@waiting} No queue in buffer."
+        sleep @interval
+        next
+      end
+
+      p "Queued in buffer. wait interval"
+      @waiting = 0
+      sleep @config.deliver['min_interval']
+
+      data.concat(@buffer.pop_all)
       list = data.map do |d|
         MForwd::Item.decode(d)
       end
-      p "#{@cnt} #{list.to_s}"
+      p "Fetched from buffer:\n  #{list}"
       messages = @item_merger.merge_into_messages list
-      p messages if messages.present?
+      p "Messages to deliver:\n  #{messages}"
       sleep @interval
     end
     Signal.trap :INT, :DEFAULT
