3aa3040056383d8b355ad23abcafba88b544ec5e
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-04 13:39:54 +0900

    Use :allow_nodev option instead of creating dummy Logger object

--- lib/mforwd/logging.rb
+++ lib/mforwd/logging.rb
@@ -6,15 +6,10 @@ module MForwd::Logging
 
     config ||= MForwd::Config.new.log
 
-    # Returns dummy logger object if no log destination available
-    if !$stdout.tty? and !config['file']
-      @logger = Class.new { def method_missing *args; nil; end }.new
-      return @logger
-    end
-
     @logger = StdLogger.create(
       config['file'],
-      shift_age: config['rotate'] || 0,
+      shift_age:   config['rotate'] || 0,
+      allow_nodev: true,
     )
     @logger.level     = Object.const_get("Logger::#{config['level']}")
     @logger.progname  = [$0, ARGV].join(%q[ ])
