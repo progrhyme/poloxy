58d56bd6b7608349fb491e9f281e10c3aea838ba
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-07 01:04:56 +0900

    Set rich format for Slack POST

--- lib/mforwd/deliver/slack.rb
+++ lib/mforwd/deliver/slack.rb
@@ -1,9 +1,29 @@
 class MForwd::Deliver::Slack < MForwd::Deliver::HttpPost
 
+  @@color_of_kind = {
+    'good'    => /^(green$|ok$|good$|recover)/i,
+    'warning' => /^(yellow$|warn)/i,
+    'danger'  => /^(red$|fatal$|crit|error$|danger$)/i,
+  }
+
   private
 
   # @param message [MForwd::Message]
   def create_body message
-    { text: message.body }.to_json
+    param = {
+      attachments: [{
+        fallback: "#{message.title}\n#{message.body}",
+        fields: [{
+          title: message.title,
+          value: message.body,
+        }],
+      }],
+    }
+    @@color_of_kind.find { |col, regexp|
+      regexp.match message.kind
+    }.tap do |col, regexp|
+      param[:attachments][0][:color] = col
+    end
+    param.to_json
   end
 end
