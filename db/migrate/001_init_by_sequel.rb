bcb1e2e3b59ec8c0d7c10230862e9a0d34a57187
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-08 13:26:13 +0900

    Don't allow null for some columns

--- db/migrate/001_init_by_sequel.rb
+++ db/migrate/001_init_by_sequel.rb
@@ -2,37 +2,38 @@ Sequel.migration do
   up do
     create_table :graph_nodes do
       primary_key :id
-      Integer :parent_id
+      Integer :parent_id, null: false
     end
 
     create_table :node_children do
-      Integer :node_id
-      Integer :child_id
+      Integer :node_id,  null: false
+      Integer :child_id, null: false
       primary_key [:node_id, :child_id]
     end
 
     create_table :messages do
       primary_key :id
-      Integer  :node_id,      index: true
-      String   :address,      size:  1024
-      String   :type,         size:  32
-      String   :kind,         size:  32
-      String   :title
-      String   :body,         text:  true
+      Integer  :node_id,      index: true, null: false
+      String   :address,      size:  1024, null: false
+      String   :type,         size:  32,   null: false
+      String   :kind,         size:  32,   null: false
+      String   :title,        null:  false
+      String   :body,         text:  true, null: false
       String   :misc,         text:  true
-      DateTime :delivered_at, index: true
+      DateTime :created_at,   index: true, null: false
+      DateTime :delivered_at
     end
 
     create_table :items do
       primary_key :id
-      Integer  :message_id,  index: true
-      String   :address,     size:  1024
-      String   :type,        size:  32
-      String   :kind,        size:  32
-      String   :name
-      String   :message,     text:  true
+      Integer  :message_id,  index: true, null: false
+      String   :address,     size:  1024, null: false
+      String   :type,        size:  32,   null: false
+      String   :kind,        size:  32,   null: false
+      String   :name,        null:  false
+      String   :message,     text:  true, null: false
       String   :misc,        text:  true
-      DateTime :received_at, index: true
+      DateTime :received_at, index: true, null: false
     end
   end
 
