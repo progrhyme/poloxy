1b387c4c46a29a795a0cbb03f52378e03e2bb2a0
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-10 01:31:03 +0900

    Add tests for DM::GraphNode#normalize_label

--- spec/data_model/graph_node_spec.rb
+++ spec/data_model/graph_node_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 describe 'Poloxy::DataModel::GraphNode' do
-  dm = Poloxy::DataModel.new
-  it 'Can load class' do
-    expect(dm.load_class 'GraphNode').to be Poloxy::DataModel::GraphNode
+  dm    = Poloxy::DataModel.new
+  klass = dm.load_class 'GraphNode'
+
+  describe '#normalize_label' do
+    {
+      ''             => nil,
+      '/ / &%$'      => nil,
+      'Foo'          => 'foo',
+      ' F oO123 '    => 'foo123',
+      '/path/to/dir' => 'pathtodir',
+      'Bar.baz-1_2'  => 'bar.baz-1_2',
+    }.each do |str, label|
+      it "'#{str}' => #{label.inspect}" do
+        expect(klass.new.send(:normalize_label, str)).to eq label
+      end
+    end
+
   end
 end
