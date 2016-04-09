721ee9b134d4f2da6203b69d5e0094cbe133e67c
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-09 09:23:17 +0900

    Update associated leaf and node when message saves

--- lib/poloxy/data_model/node_leaf.rb
+++ lib/poloxy/data_model/node_leaf.rb
@@ -1,2 +1,11 @@
 class Poloxy::DataModel::NodeLeaf < Sequel::Model(:node_leaves)
+  def self.create_or_update cond, params
+    me = find cond
+    if me
+      me.update params
+    else
+      props = cond.merge params
+      self.dataset.insert props
+    end
+  end
 end
