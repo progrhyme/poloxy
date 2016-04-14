edb5e79b928ded50bda1ceb0e343e19f2fe13096
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-14 09:14:45 +0900

    Add ItemMerge::MessageContainer for message merging

---
+++ lib/poloxy/item_merge/message_container.rb
@@ -0,0 +1,20 @@
+class Poloxy::ItemMerge::MessageContainer
+  attr :messages, :level, :total_num, :item_num
+  def initialize config=nil, args={}
+    @messages  = args[:messages] || []
+    @level     = args[:level]    || Poloxy::MIN_LEVEL
+    @total_num = args[:count]    || 0
+    @item_num  = args[:item_num] || 0
+  end
+
+  def merge other
+    @messages.concat other.messages
+    @level     =  [@level, other.level].max
+    @total_num += other.total_num
+    @item_num  += other.item_num
+  end
+
+  def append msg
+    @messages << msg
+  end
+end
