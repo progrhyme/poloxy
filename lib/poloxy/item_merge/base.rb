dbabddb4229d3bb5982e00f98987cb92dde20618
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-30 21:40:08 +0900

    Move #item2msg method from P::IM::PerItem => P::IM::Base
    - Prepare for PerGroup merge
    - Using #abbrev_with_level method in P::Function::Group

--- lib/poloxy/item_merge/base.rb
+++ lib/poloxy/item_merge/base.rb
@@ -1,4 +1,6 @@
 class Poloxy::ItemMerge::Base
+  include Poloxy::ViewHelper
+
   def initialize config: nil
     @config     = config || Poloxy::Config.new
     @data_model = Poloxy::DataModel.new
@@ -16,30 +18,86 @@ class Poloxy::ItemMerge::Base
 
   private
 
-    def config
-      @config
-    end
+  def config
+    @config
+  end
 
-    # @param list [Array<Poloxy::DataModel::Item>]
-    def pre_merge_items list
-      items = {}
-      list.each do |i|
-        groups = i.group.split(@config.graph['delimiter'])
-        items[i.type]            ||= {}
-        items[i.type][i.address] ||= {}
-        bucket = items[i.type][i.address]
-        groups.each do |grp|
-          bucket = bucket[grp] ||= {}
-        end
-        bucket[:items]                  ||= {}
-        bucket[:items][i.name]          ||= {}
-        bucket[:items][i.name][i.level] ||= []
-        bucket[:items][i.name][i.level] <<  i
+  # @param list [Array<Poloxy::DataModel::Item>]
+  def pre_merge_items list
+    items = {}
+    list.each do |i|
+      groups = i.group.split(@config.graph['delimiter'])
+      items[i.type]            ||= {}
+      items[i.type][i.address] ||= {}
+      bucket = items[i.type][i.address]
+      groups.each do |grp|
+        bucket = bucket[grp] ||= {}
       end
-      items
+      bucket[:items]                  ||= {}
+      bucket[:items][i.name]          ||= {}
+      bucket[:items][i.name][i.level] ||= []
+      bucket[:items][i.name][i.level] <<  i
+    end
+    items
+  end
+
+  def merge_items items
+    raise Poloxy::Error, 'Please override in subclass!'
+  end
+
+  # @param name [String] {Poloxy::DataModel::Item#name}
+  # @param items [Hash{String => Array<Poloxy::DataModel::Item>}]
+  #  key: {Poloxy::DataModel::Item#level}
+  def items2msg name, items
+    params = {
+      'item'        => name,
+      'items'       => [],
+    }
+
+    items.first[1].first.tap do |item|
+      %w[group type address].each do |key|
+        params[key] = item.send(key)
+      end
     end
 
-    def merge_items items
-      raise Poloxy::Error, 'Please override in subclass!'
+    levels   = items.keys
+    worst_lv = levels.max
+    worst_label = abbrev_with_level worst_lv
+    params['level'] = worst_lv
+    params['title'] = '%s / %s' % [worst_label, name]
+
+    if levels.size == 1
+      list = params['items'] = items.first[1]
+      params['expire_at'] = list.map(&:expire_at).max
+      params['body']  = <<EOB
+# #{list.length} items.
+# Latest message:
+#{list.last.message}
+EOB
+    else
+      nums  = {}
+      total = 0
+      params['expire_at'] = Time.now
+      levels.sort.reverse.each do |lv|
+        label       =  abbrev_with_level lv
+        list        =  items[lv]
+        nums[label] =  list.length
+        total       += list.length
+        params['items'].concat(list)
+        params['expire_at'] = list.map(&:expire_at).push( params['expire_at'] ).max
+      end
+      params['body'] = ERB.new(<<EOB, nil, '-').result(binding)
+# There are <%= total %> items of <%= nums.size %> levels:
+<% nums.each do |label, num| -%>
+#   <%= "%-7s" % [label] %> => <%= num %>
+<% end -%>
+# ----
+# Latest message at `<%= worst_label %>`:
+<%= items[worst_lv].last.message %>
+EOB
     end
+
+    params['created_at'] = Time.now
+    @data_model.spawn 'Message', params
+  end
 end
--- lib/poloxy/item_merge/per_item.rb
+++ lib/poloxy/item_merge/per_item.rb
@@ -1,5 +1,4 @@
 class Poloxy::ItemMerge::PerItem < Poloxy::ItemMerge::Base
-  include Poloxy::ViewHelper
 
   private
 
@@ -35,67 +34,5 @@ class Poloxy::ItemMerge::PerItem < Poloxy::ItemMerge::Base
       mcontainer.append items2msg(name, stash)
     end
     mcontainer
-  end
-
-  # @param name [String] {Poloxy::DataModel::Item#name}
-  # @param items [Hash{String => Array<Poloxy::DataModel::Item>}]
-  #  key: {Poloxy::DataModel::Item#level}
-  def items2msg name, items
-    params = {
-      'item'  => name,
-      'items' => [],
-    }
-
-    items.first[1].first.tap do |item|
-      %w[group type address expire_at].each do |key|
-        params[key] = item.send(key)
-      end
-    end
-
-    lv2label = lambda do |lv|
-      abbrev, _lv = abbrev_and_level lv
-      if lv == _lv
-        abbrev
-      else
-        "%s(Lv%d)" % [abbrev, lv]
-      end
-    end
-
-    levels   = items.keys
-    worst_lv = levels.max
-    worst_label = lv2label.call worst_lv
-    params['level'] = worst_lv
-    params['title'] = '%s / %s' % [worst_label, name]
-
-    if levels.size == 1
-      list = params['items'] = items.first[1]
-      params['body']  = <<EOB
-# #{list.length} items.
-# Latest message:
-#{list.last.message}
-EOB
-    else
-      nums  = {}
-      total = 0
-      levels.sort.reverse.each do |lv|
-        label       =  lv2label.call lv
-        list        =  items[lv]
-        nums[label] =  list.length
-        total       += list.length
-        params['items'].concat(list)
-      end
-      params['body'] = ERB.new(<<EOB, nil, '-').result(binding)
-# There are <%= total %> items of <%= nums.size %> levels:
-<% nums.each do |label, num| -%>
-#   <%= "%-7s" % [label] %> => <%= num %>
-<% end -%>
-# ----
-# Latest message at `<%= worst_label %>`:
-<%= items[worst_lv].last.message %>
-EOB
-    end
-
-    params['created_at'] = Time.now
-    @data_model.spawn 'Message', params
   end
 end
